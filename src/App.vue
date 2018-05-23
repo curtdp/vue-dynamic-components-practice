e4b64dea2ccb301db8bfb4ae13d36231c26d5185
Igor Oliinyk <igor.oleynikdp@gmail.com>
2018-05-24 00:14:28 +0300

    Сделал загрузку динамических компонентов

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app" class="container mx-auto">
     <div class="w-full my-8">
-      <button class="py-2 px-4 rounded border border-blue bg-blue hover:bg-blue-dark mr-4 text-white">Синий шаблон</button>
-      <button class="py-2 px-4 rounded border border-green bg-green hover:bg-green-dark mr-4 text-white">Зеленый шаблон</button>
-      <button class="py-2 px-4 rounded border border-red bg-red hover:bg-red-dark mr-4 text-white">Красный шаблон</button>
+      <button @click="selectedComponent = 'Blue'" class="py-2 px-4 rounded border border-blue bg-blue hover:bg-blue-dark mr-4 text-white">Синий шаблон</button>
+      <button @click="selectedComponent = 'Green'" class="py-2 px-4 rounded border border-green bg-green hover:bg-green-dark mr-4 text-white">Зеленый шаблон</button>
+      <button @click="selectedComponent = 'Red'" class="py-2 px-4 rounded border border-red bg-red hover:bg-red-dark mr-4 text-white">Красный шаблон</button>
       <hr class="border border-grey">
+      <component :is="selectedComponent">
+        <p>Это какой-то контент</p>
+      </component>
 
-      <Blue>
+      <!-- <Blue>
           <p>Это синий контент</p>
       </Blue>
       <Green>
@@ -14,7 +17,7 @@
       </Green>
       <Red>
         <p>Это красный контент</p>
-      </Red>
+      </Red> -->
 
     </div>
 
@@ -29,7 +32,7 @@ export default {
   name: 'app',
   data() {
     return {
-
+      selectedComponent: 'Blue'
     }
   },
   components: {
